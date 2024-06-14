1d03ccbcceede4be495d0c80e4c1a8b11eed5c9a
VieraJulian <julianviera2019@hotmail.com>
2024-06-14 09:00:39 -0300

    Empezando uno de los dos ejercicios practicos del curso Spring Security

---
+++ educationalplatform/src/main/java/com/CourseTodoCode/educationalplatform/EducationalplatformApplication.java
@@ -0,0 +1,13 @@
+package com.CourseTodoCode.educationalplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationalplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationalplatformApplication.class, args);
+	}
+
+}
---
+++ educationalplatform/src/test/java/com/CourseTodoCode/educationalplatform/EducationalplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CourseTodoCode.educationalplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationalplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
