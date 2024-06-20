edf96b58ad5765ef320a07bf6c76587de245eabd
VieraJulian <julianviera2019@hotmail.com>
2024-06-20 11:03:07 -0300

    Método para extraer un claim específico de un token terminado

--- educationalplatform/src/main/java/com/CourseTodoCode/educationalplatform/utils/JwtUtils.java
+++ educationalplatform/src/main/java/com/CourseTodoCode/educationalplatform/utils/JwtUtils.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
@@ -60,6 +61,10 @@ public class JwtUtils {
     }
 
     public String extractUsername(DecodedJWT decodedJWT){
-        return decodedJWT.getSignature().toString();
+        return decodedJWT.getSubject().toString();
+    }
+
+    public Claim getSpecificClaim(DecodedJWT decodedJWT, String claimName){
+        return decodedJWT.getClaim(claimName);
     }
 }
